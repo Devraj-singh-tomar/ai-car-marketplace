ad5a005748cb2303ceeeb7f3e87e5bdf1bf68c7a
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-06-29 14:07:46 +0530

    feat: add user and account models with authentication setup using NextAuth and Prisma

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,12 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { prisma } from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub],
+  session: {
+    strategy: "jwt",
+  },
 });
